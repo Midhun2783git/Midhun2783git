c0c12bc9b77168df7aa63f25f3fe5a3216a52529
Midhun2783git <99118893+Midhun2783git@users.noreply.github.com>
2024-12-28 16:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const text = "HI, I am MIDHUN";
-    const animatedText = document.getElementById("animatedText");
-    let index = 0;
+// document.addEventListener("DOMContentLoaded", function() {
+//     const text = "HI, I am MIDHUN";
+//     const animatedText = document.getElementById("animatedText");
+//     let index = 0;
 
-    function showText() {
-        if (index < text.length) {
-            animatedText.innerHTML += text.charAt(index);
-            index++;
-            setTimeout(showText, 200); // Adjust the speed here
-        } else {
-            animatedText.style.opacity = 1; // Fade in the text
-        }
-    }
+//     function showText() {
+//         if (index < text.length) {
+//             animatedText.innerHTML += text.charAt(index);
+//             index++;
+//             setTimeout(showText, 200); // Adjust the speed here
+//         } else {
+//             animatedText.style.opacity = 1; // Fade in the text
+//         }
+//     }
 
-    showText();
-});
+//     showText();
+// });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
